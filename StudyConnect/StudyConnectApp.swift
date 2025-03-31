c913ca8fa7db22afdf90b42714bc0038e1313846
MenukaRanasinghe <hgmranasinghe@gmail.com>
2025-03-31 14:51:29 +0530

    Initial Commit

---
+++ StudyConnect/StudyConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyConnectApp.swift
+//  StudyConnect
+//
+//  Created by Menuka 046 on 2025-03-31.
+//
+
+import SwiftUI
+
+@main
+struct StudyConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudyConnectTests/StudyConnectTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudyConnectTests.swift
+//  StudyConnectTests
+//
+//  Created by Menuka 046 on 2025-03-31.
+//
+
+import Testing
+@testable import StudyConnect
+
+struct StudyConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
