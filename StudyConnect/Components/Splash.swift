10cdf245ed36c90b83f459528603a99c4ad80951
MenukaRanasinghe <hgmranasinghe@gmail.com>
2025-04-09 12:14:19 +0530

    OnBoarding Screens done

--- StudyConnect/Components/Splash.swift
+++ StudyConnect/Components/Splash.swift
@@ -9,23 +9,36 @@ import SwiftUI
 
 struct Splash: View {
     @State private var animate = false
+    @State private var navigateToOnboarding = false
 
     var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
-            
-            VStack {
-                Image("Logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 150, height: 150)
-                    .scaleEffect(animate ? 1 : 0.5)
-                    .opacity(animate ? 1 : 0)
-                    .onAppear {
-                        withAnimation(.easeOut(duration: 1.2)) {
-                            animate = true
+        NavigationView {
+            ZStack {
+                Color.black.ignoresSafeArea()
+                
+                VStack {
+                    Image("Logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 150)
+                        .scaleEffect(animate ? 1 : 0.5)
+                        .opacity(animate ? 1 : 0)
+                        .onAppear {
+                            withAnimation(.easeOut(duration: 1.2)) {
+                                animate = true
+                            }
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
+                                navigateToOnboarding = true
+                            }
                         }
-                    }
+
+                    NavigationLink(
+                        destination: OnboardingView(),
+                        isActive: $navigateToOnboarding,
+                        label: { EmptyView() }
+                    )
+                }
             }
         }
     }
